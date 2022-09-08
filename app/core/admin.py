72d6fdd1d0a2cfc8288ed6df43584b1095943080
Michelle Perini <michellep@thestowcompany.com>
2022-09-08 12:51:03 -0400

    Finished Admin Page

--- app/core/admin.py
+++ app/core/admin.py
@@ -12,8 +12,10 @@ class UserAdmin(BaseUserAdmin):
     #support for modifying users
     fieldsets = ( 
         (None, {'fields': ('email', 'password')}),
-        (
-            _('Permissions'), {
+        (_('Personal Info'), {'fields': ('name',)}),
+        (	
+            _('Permissions'), 
+            {
                 'fields': (
                     'is_active',
                     'is_staff',
@@ -23,7 +25,20 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
-
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes' : ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -27,6 +27,13 @@ class AdminSiteTests(TestCase):
 
         #Ensure pages loaded correctly
         self.assertEqual(res.status_code, 200)
-        
+
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add') #defined by django admin
+        res = self.client.get(url)
+
+        #Ensure pages loaded correctly
+        self.assertEqual(res.status_code, 200)
+
 
 
